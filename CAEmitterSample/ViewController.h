9a57747bb02eb7f7ba79c871f1bbdc828c06f366
shu223 <shuichi0526@gmail.com>
2013-03-12 11:19:56 +0900

    Initial Commit

---
+++ CAEmitterSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CAEmitterSample
+//
+//  Created by shuichi on 13/03/12.
+//  Copyright (c) 2013年 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CAEmitterSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CAEmitterSample
+//
+//  Created by shuichi on 13/03/12.
+//  Copyright (c) 2013年 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
